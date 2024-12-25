021b7f4e34f5fa5f33bdb89278cb71685a8461e2
vaghelakrishna <vaghelakrishna523@gmail.com>
2024-12-25 23:08:13 +0530

    Create bankAccount.java

---
+++ ATM Interface/bankAccount.java
@@ -0,0 +1,28 @@
+
+public class bankAccount {
+
+    double balance;
+
+    public bankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+}
